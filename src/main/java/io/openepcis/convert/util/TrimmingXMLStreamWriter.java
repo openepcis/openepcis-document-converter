f6c481c4bc8846d289054efe4b2951013e8e55ee
Sven Boeckelmann <sboeckelmann@benelog.com>
2023-02-19 12:46:42 +0100

    add trimming XML stream writer

---
+++ src/main/java/io/openepcis/convert/util/TrimmingXMLStreamWriter.java
@@ -0,0 +1,21 @@
+package io.openepcis.convert.util;
+
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+public class TrimmingXMLStreamWriter extends DelegatingXMLStreamWriter {
+    public TrimmingXMLStreamWriter(XMLStreamWriter writer) {
+        super(writer);
+    }
+
+    @Override
+    public void writeCharacters(String text) throws XMLStreamException {
+        super.writeCharacters(text.trim());
+    }
+
+    @Override
+    public void writeCharacters(char[] text, int start, int len) throws XMLStreamException {
+        super.writeCharacters(new String(text, start, len).trim());
+    }
+
+}

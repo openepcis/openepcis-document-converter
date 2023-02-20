7d3f6c4f2877c80dd50eff6b4ca292b1865fc356
Sven Boeckelmann <sboeckelmann@benelog.com>
2023-02-20 09:12:08 +0100

    code format

--- src/main/java/io/openepcis/convert/util/TrimmingXMLStreamWriter.java
+++ src/main/java/io/openepcis/convert/util/TrimmingXMLStreamWriter.java
@@ -4,18 +4,17 @@ import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
 
 public class TrimmingXMLStreamWriter extends DelegatingXMLStreamWriter {
-    public TrimmingXMLStreamWriter(XMLStreamWriter writer) {
-        super(writer);
-    }
+  public TrimmingXMLStreamWriter(XMLStreamWriter writer) {
+    super(writer);
+  }
 
-    @Override
-    public void writeCharacters(String text) throws XMLStreamException {
-        super.writeCharacters(text.trim());
-    }
+  @Override
+  public void writeCharacters(String text) throws XMLStreamException {
+    super.writeCharacters(text.trim());
+  }
 
-    @Override
-    public void writeCharacters(char[] text, int start, int len) throws XMLStreamException {
-        super.writeCharacters(new String(text, start, len).trim());
-    }
-
+  @Override
+  public void writeCharacters(char[] text, int start, int len) throws XMLStreamException {
+    super.writeCharacters(new String(text, start, len).trim());
+  }
 }
